2038659e71f100399a2c4a138d6a504476794669
sndrrt <sanderrietdijk@gmail.com>
2025-06-07 13:39:35 +0200

    Update recommend.js

--- api/recommend.js
+++ api/recommend.js
@@ -1,6 +1,9 @@
 export default async function handler(req) {
   return new Response(
-    JSON.stringify({ status: "✅ Function draait", env: process.env.TMDB_KEY ? "✅ KEY aanwezig" : "❌ KEY ontbreekt" }),
-    { headers: { "Content-Type": "application/json" } }
+    JSON.stringify({ message: "✅ Basic function werkt!" }),
+    {
+      headers: { "Content-Type": "application/json" },
+      status: 200
+    }
   );
 }
