48d36060c41ccb814bb22b1c54244e312a7a9db1
sndrrt <sanderrietdijk@gmail.com>
2025-06-07 10:22:18 +0200

    Delete functions/seen.js

--- functions/seen.js
+++
@@ -1,8 +0,0 @@
-// Slaat een titel op in de 'gezien'-lijst
-import { kv } from "@vercel/kv";
-
-export default async req => {
-  const { imdb_id, user = "anon" } = await req.json();
-  await kv.sadd(`seen:${user}`, imdb_id);
-  return new Response("OK");
-};
